48046e1ff0e7a12f3cc4074c849befce135337c9
Oliver Werlen <oliverwerlen@bluewin.ch>
2021-05-19 17:18:14 +0200

    added map functionality

--- app/src/main/java/com/example/jtipickup/ui/login/SessionManager.kt
+++ app/src/main/java/com/example/jtipickup/ui/login/SessionManager.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.content.SharedPreferences
 import android.system.Os.remove
 import com.example.jtipickup.R
+import com.example.jtipickup.response.PickUpResponse
+import com.google.gson.Gson
 
 class SessionManager (context: Context){
     private var prefs: SharedPreferences = context.getSharedPreferences(context.getString(R.string.app_name), Context.MODE_PRIVATE)
 
     companion object {
         const val USER_TOKEN = "user_token"
+        const val PICKUP_SELECTED = "pickup_selected"
     }
 
     /**
@@ -19,6 +22,23 @@ class SessionManager (context: Context){
         val editor = prefs.edit()
         editor.putString(USER_TOKEN, token)
         editor.apply()
+    }
+
+    /**
+     * Function to save pickupstation token
+     */
+    fun savePickUpSelected(token: PickUpResponse) {
+        val editor = prefs.edit()
+        val gson = Gson()
+        editor.putString(PICKUP_SELECTED, gson.toJson(token))
+        editor.apply()
+    }
+    /**
+     * Function to fetch auth token
+     */
+    fun fetchPickUpSelected(): PickUpResponse? {
+        val gson = Gson()
+        return gson.fromJson(prefs.getString(PICKUP_SELECTED, null), PickUpResponse::class.java)
     }
 
     /**
